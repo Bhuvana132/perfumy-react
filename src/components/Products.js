0583d1ffa4c6ed9fc0f839664c2309cbb674defd
Bhuvana132 <bhuvanashree231@gmail.com>
2025-03-27 16:41:44 +0530

    Fix line endings

---
+++ src/components/Products.js
@@ -0,0 +1,36 @@
+import one from "../assets/images/one.jpg"
+import two from "../assets/images/two.jpg"
+import three from "../assets/images/three.jpg"
+
+
+//Product Component
+function Product()
+{
+  return(
+    <div class="products">
+          
+            <div class="box">
+                
+            <img src={one} alt="Perfume Image" />
+
+                        <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p>    
+            </div>
+                
+            
+            <div class="box">
+            <img src={two} alt="Perfume Image" />
+
+                <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p> 
+            </div>
+            <div class="box">
+            <img src={three} alt="Perfume Image" />
+
+                <p>Designer Club-Be Spoke by Perfume Lounge Men's Perfume | Premium Long Lasting Perfume</p>
+            </div>
+
+           
+        </div>
+  )
+}
+
+export default Product;
---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+//Search Component
+function Search()
+{
+  return(
+    <div className="searchbar">
+       <input type="text" placeholder="Search Perfume"/>
+    </div>
+  )
+}
+
+export default Search
